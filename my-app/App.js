6a1bec87a0d3eeb75d53238b76ddbf5505d090d4
kaycarls <syafiq.awangku@hotmail.com>
2023-04-05 17:24:24 +0800

    modified:   my-app/App.js 	new file:   my-app/assets/ulu1.jpg 	new file:   my-app/assets/ulu2.jpg 	new file:   my-app/assets/ulu3.jpg 	new file:   my-app/assets/ulu4.jpg 	new file:   my-app/assets/ulu5.jpg 	modified:   my-app/package-lock.json 	modified:   my-app/package.json 	modified:   my-app/screens/Carousel.js 	new file:   my-app/screens/PackageDetails1.js

--- my-app/App.js
+++ my-app/App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import MainScreen from './screens/MainScreen';
+import PackageDetails1 from './screens/PackageDetails1';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Main" component={MainScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="PackageDetails1" component={PackageDetails1} options={{  headerTitle: 'Package Details' , headerShown: true }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- my-app/screens/Carousel.js
+++ my-app/screens/Carousel.js
@@ -1,27 +1,37 @@
 import React from 'react';
-import { View, ScrollView, Text, Image, StyleSheet } from 'react-native';
+import { View, ScrollView, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 const items = [
-  { id: 1, name: 'Tour Package 1', image: require('../assets/image1.png'), agency: 'By Travel Agency', description: 'Description of Item 1' },
-  { id: 2, name: 'Tour Package 2', image: require('../assets/image2.jpg'), agency: 'By Travel Agency', description: 'Description of Item 2' },
-  { id: 3, name: 'Tour Package 3', image: require('../assets/image3.jpg'), agency: 'By Travel Agency', description: 'Description of Item 3' },
+  { id: 1, name: 'Complete Rainforest Experience', image: require('../assets/ulu1.jpg'), agency: 'By Travel Agency', description: '', layout: 'PackageDetails1' },
+  { id: 2, name: 'Tour Package 2', image: require('../assets/image1.png'), agency: 'By Travel Agency', description: 'Description of Item 1', layout: 'PackageDetails1' },
+  { id: 3, name: 'Tour Package 3', image: require('../assets/image2.jpg'), agency: 'By Travel Agency', description: 'Description of Item 2', layout: 'PackageDetails1' },
+  { id: 4, name: 'Tour Package 4', image: require('../assets/image3.jpg'), agency: 'By Travel Agency', description: 'Description of Item 3', layout: 'PackageDetails1' },
 ];
 
-const CarouselItem = ({ item }) => {
+const CarouselItem = ({ item, onPress }) => {
   return (
-    <View style={styles.itemContainer}>
+    <TouchableOpacity style={styles.itemContainer} onPress={onPress}>
       <Image source={item.image} style={styles.itemImage} />
       <Text style={styles.itemName}>{item.name}</Text>
       <Text style={styles.itemAgency}>{item.agency}</Text>
       <Text style={styles.itemDescription}>{item.description}</Text>
-    </View>
+    </TouchableOpacity>
   );
 };
 
 const Carousel = () => {
+  const navigation = useNavigation();
+
+  const handlePress = (layout) => {
+    navigation.navigate(layout);
+  };
+
   return (
     <ScrollView horizontal={true} style={styles.carouselContainer}>
-      {items.map(item => <CarouselItem item={item} key={item.id} />)}
+      {items.map(item => (
+        <CarouselItem item={item} key={item.id} onPress={() => handlePress(item.layout)} />
+      ))}
     </ScrollView>
   );
 };
